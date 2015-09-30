1a6aeea2deb963aeaf9523b7fa4d2979df4e6306
cminhho <hmchung@tma.com.vn>
2015-09-30 15:20:24 +0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var generators = require('yeoman-generator');
+module.exports = generators.Base.extend(
+	// The name 'constructor' is important here
+	constructor: function(){
+		// Calling the super constructer is important so our generator is correctly set up
+		generators.Base.apply(this, arguments);
+		
+		// Next, add your custom code
+		this.option('coffee'); // This method adds support for a '--coffee' flag
+	},
+	method1: function(){
+		console.log('Method 1 just ran');
+	},
+	method2: function(){
+		console.log('Method 2 just ran');
+	}
+);
